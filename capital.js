b5a732b0564c1b51bc9e8cdc3f0cef1a48818c13
Mysterio-O <skrabbi.019gmail.com>
2025-08-18 01:16:27 +0600

    added capitalize first letter function

---
+++ capital.js
@@ -0,0 +1,27 @@
+// Capitalize First Letter of Each Word
+
+function capitalizeFirstWord(str) {
+    // console.log(str);
+
+    // check if it is string or not
+    if (typeof str !== 'string') return 'input must be a string';
+
+    // split the sting into spaces
+    const words = str.split(' ');
+    // console.log(words);
+
+    let final = []
+
+    // loop through the array
+    for (const word of words) {
+        // console.log(word);
+        const capitalized = word[0].toUpperCase() + word.slice(1).toLowerCase();
+        final.push(capitalized);
+
+    }
+
+    return final.join(' ');
+
+}
+
+console.log(capitalizeFirstWord('hello world'));
