37be43885af597a62643b676696a7ec973def00f
Mysterio-O <skrabbi.019gmail.com>
2025-08-18 00:06:26 +0600

    added max number function

--- revert.js
+++ revert.js
@@ -2,7 +2,7 @@
 
 /**
  * this function takes string as output.
- * first it checks if the input is string or not, if it is string then it will execute the process or else, it will avoid the process
+ * first it checks if the input type is string or not, if it is string then it will execute the process or else, it will avoid the process
  * 
  * at first it splits the string into an array and then loop through the array. in that loop, the function unshift every element of that array into another array. unshift basically push element from the back side and it reverse the words order. then the function join those words into a string. and it the the reversed string
  *  
@@ -10,7 +10,7 @@
 
 function fnReverse(str) {
     // console.log(str)
-    if (str === 'string') {
+    if (typeof str === 'string') {
         // splitting the string and turning into an array
         const strArr = str.split('');
         // console.log(strArr);
@@ -25,11 +25,13 @@ function fnReverse(str) {
         // console.log(array);
 
         const finalOutput = array.join(' ');
-        console.log(finalOutput);
+        // console.log(finalOutput);
+        return finalOutput
     }
     else {
-        console.log('input must be a string');
+        // console.log('input must be a string');
+        return 'input must be a string'
     }
 }
 
-fnReverse(['Hello World'])
+console.log(fnReverse('Hello World'))
